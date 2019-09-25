f8364b3205df329440a0ae7f48c91ea9da6f2f8c
GABRIEL DE SALLES BRUNICHAKI <17111028@pucrs.br>
2019-09-25 20:51:04 -0300

    Some file methods added

--- persistentbean/Persistence.java
+++ persistentbean/Persistence.java
@@ -1,9 +1,64 @@
 package persistentbean;
  //Feijão Persistente
 
+ import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Persistence {
-    public String getName() {
-        return "Teste";
+  private String file;
+
+  public Persistence() {
+    this.file = null;
+  }
+
+  public String getFile() {
+    return this.file;
+  }
+
+  public void setFile(String file) {
+    this.file = file;
+  }
+
+  public boolean fileExists() {
+    File file = new File(this.file);
+    return file.exists();
+  }
+
+  public void createFile() {
+    File file = new File(this.file);
+
+    if (!fileExists()) {
+      file.createNewFile();
     }
+  }
+
+  public ArrayList<String> readFile() {
+    try {
+      FileReader textFile = new FileReader(this.file);
+      BufferedReader readFile = new BufferedReader(textFile);
+
+      ArrayList<String> lines = new ArrayList<String>();
+
+      while (readFile.ready()) {
+        lines.add(readFile.readLine());
+      }
+
+      textFile.close();
+
+      return lines;
+    } catch (IOException e) {
+      System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+    }
+
+  }
+
+  public void writeFile() {
+
+    FileWriter arq = new FileWriter("d:\\tabuada.txt");
+    PrintWriter gravarArq = new PrintWriter(arq);
+  }
+
 }

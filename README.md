7eccebbc9b51e041470f5049645e66c712b76971
gabrielweich <gabrielweich.dev@gmail.com>
2019-09-29 20:58:10 -0300

    Added documentation

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 "# javabean-component" 
+
+# Documentation
+
+## `public Persistence()`
+
+Construtor
+
+ * **Returns:** 
+
+## `public String getFile()`
+
+ * **Returns:** String - caminho do arquivo
+
+## `public void setFile(String file) throws IOException`
+
+Altera o arquivo onde os dados serão persistidos
+
+ * **Parameters:** `file` — String - Caminho ou nome do arquivo
+ * **Exceptions:** `IOException` — 
+
+## `public String getProperty(String key)`
+
+Busca o valor associado à determinada chave
+
+ * **Parameters:** `key` — String - chave
+ * **Returns:** String - valor
+
+## `public void setProperty(String key, String value) throws IOException`
+
+Associa um valor a uma determinada chave.
+
+ * **Parameters:**
+   * `key` — String chave
+   * `value` — String valor
+ * **Exceptions:** `IOException` — 
--- persistentbean/Persistence.java
+++ persistentbean/Persistence.java
@@ -12,21 +12,35 @@ public class Persistence {
   private String file;
   private Properties props;
 
+  
+  /** 
+   * Construtor
+   * @return 
+   */
   public Persistence() {
     this.file = null;
     this.props = null;
   }
 
+  /** 
+   * @return String - caminho do arquivo
+   */
   public String getFile() {
     return this.file;
   }
 
+  
+  /** 
+   * Altera o arquivo onde os dados serão persistidos
+   * @param file String - Caminho ou nome do arquivo
+   * @throws IOException
+   */
   public void setFile(String file) throws IOException {
     this.file = file;
     this.syncProps();
   }
 
-  public File syncFile() throws IOException {
+  private File syncFile() throws IOException {
     File file = new File(this.file);
     if (!file.exists())
       file.createNewFile();
@@ -34,23 +48,36 @@ public class Persistence {
     return file;
   }
 
-  public void syncProps() throws IOException {
+  private void syncProps() throws IOException {
     this.syncFile();
     Properties props = new Properties();
     props.load(new FileInputStream(this.file));
     this.props = props;
   }
 
+  
+  /** 
+   * Busca o valor associado à determinada chave
+   * @param key String - chave
+   * @return String - valor
+   */
   public String getProperty(String key) {
     return this.props.getProperty(key);
   }
 
+  
+  /** 
+   * Associa um valor a uma determinada chave.
+   * @param key String chave
+   * @param value String valor
+   * @throws IOException
+   */
   public void setProperty(String key, String value) throws IOException {
     this.props.setProperty(key, value);
     this.saveProperties();
   }
 
-  public void saveProperties() throws IOException {
+  private void saveProperties() throws IOException {
     FileOutputStream fr = new FileOutputStream(this.file);
     this.props.store(fr, "Properties");
     fr.close();
